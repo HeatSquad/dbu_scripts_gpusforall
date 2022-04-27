85ca0175923912aed68c1745f3ec656edcc55f1c
Thuc Nguyen <thucn2@uci.edu>
2022-04-26 22:43:43 -0700

    GPUS-58 Pushing changes to users table: deleted created_by and added triggers

--- dbu_users.sql
+++ dbu_users.sql
@@ -10,9 +10,31 @@ CREATE TABLE users
     json_address    JSON,
     json_user       JSON,
     created		    DATETIME        NOT NULL,
-    created_by	    VARCHAR(64)     NOT NULL,
     modified		DATETIME        NOT NULL,
     modified_by	    VARCHAR(64)     NOT NULL,
     deleted 		VARCHAR(1)		DEFAULT 'N',
     PRIMARY KEY (userid)
 );
+
+-- ========================================================
+-- Triggers
+-- ========================================================
+DELIMITER //
+CREATE TRIGGER sys_users_insert_trigger_1
+BEFORE INSERT ON users
+FOR EACH ROW 
+BEGIN
+    SET NEW.password_reset = CURRENT_TIMESTAMP;
+    SET NEW.created = CURRENT_TIMESTAMP;
+    SET NEW.modified = CURRENT_TIMESTAMP;
+END //
+
+CREATE TRIGGER sys_users_update_trigger_2
+BEFORE UPDATE ON users
+FOR EACH ROW 
+BEGIN
+    SET NEW.modified = CURRENT_TIMESTAMP;
+END //
+
+/* DELIMITER set back to default*/
+DELIMITER ;
