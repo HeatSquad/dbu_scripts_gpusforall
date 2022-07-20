f64b16ba942ae494ff41948bba827c1f6be6232e
Thuc Nguyen <thucn2@uci.edu>
2022-07-19 21:05:22 -0700

    GPUS-19 Updated users table

--- dbu_users.sql
+++ dbu_users.sql
@@ -1,17 +1,20 @@
 CREATE TABLE users 
 (
     userid 		    VARCHAR(64)		NOT NULL,
-    email		    VARCHAR(500)	NOT NULL,
-    first_name		VARCHAR(64)		NOT NULL,
-    last_name		VARCHAR(64)		NOT NULL,
+    username        VARCHAR(30)     NOT NULL,
+    email		    VARCHAR(320)	NOT NULL,
+    first_name		VARCHAR(100)	NOT NULL,
+    last_name		VARCHAR(100)	NOT NULL,
     dob             DATE            NOT NULL,
-    password		VARCHAR(500)    NOT NULL,
+    password		VARCHAR(64)     NOT NULL,
     password_reset  DATETIME        NOT NULL,
+    salt            VARCHAR(64)     NOT NULL,
+    verified        BOOLEAN NOT NULL DEFAULT FALSE,
     json_address    JSON,
     json_user       JSON,
     created		    DATETIME        NOT NULL,
     modified		DATETIME        NOT NULL,
-    modified_by	    VARCHAR(64)     NOT NULL,
+    modified_by	    VARCHAR(30)     NOT NULL,
     deleted 		VARCHAR(1)		DEFAULT 'N',
     PRIMARY KEY (userid)
 );
